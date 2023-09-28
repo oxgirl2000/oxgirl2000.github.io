fa7b7e07df1252c9ddfadb0323175cec7f3dd939
ox <75142042+oxgr@users.noreply.github.com>
2023-09-28 04:43:27 -0400

    dynamic routing base

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const projectCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    year: z.number(),
+    description: z.string(),
+    collaborator: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'projects': projectCollection,
+};
---
+++ src/content/projects/spherePackingBach.md
@@ -0,0 +1,14 @@
+---
+title: Sphere Packing Bach
+year: 2022
+description: 1024 speakers lining the interior of a 3-meter wide sphere.
+collaborator: Rafael Lozano-Hemmer
+tags: [ tools, hardware ]
+---
+hi mom hi mom
+
+hi mom
+hi mom
+hi mom
+hi mom
+
